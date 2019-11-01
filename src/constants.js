5c76d64b4ebfdce3d8dddab47e7aa4cad10895ee
Tubt <tu.bui@gooddata.com>
2019-11-01 15:12:42 +0700

    yarn add cross-env

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // module.exports is important for setupProxy.js
 module.exports = {
     appName: "My App",
-    backend: "https://secure.gooddata.com",
-    projectId: "la84vcyhrq8jwbu4wpipw66q2sqeb923",
+    backend: "https://zebroids.intgdc.com/",
+    projectId: "ltn06hvt07uko2r87itmnoaibgzc0mkn",
 };
